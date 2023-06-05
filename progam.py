ffef239f8303cbfe5d5f6c7ae0da231f183ead6b
Nidhomun <fitrinidhomun@gmail.com>
2023-06-05 11:45:15 +0700

    1

--- progam.py
+++ progam.py
@@ -1,42 +1,65 @@
 #ini bikinya disini ya ges
 #SEMANGATTT!!
 
-def login():
-    email = input ("masukkan email anda:")
-    username = input("Masukkan username: ")
-    password = input("Masukkan password: ")
+import csv
 
-    # Cek username dan password
-    if email == "@gmail.com" and username == "user" and password == "password":
-        return True
-    else:
-        return False
+def load_user_data():
+    # Load user data from the CSV file
+    with open('datauser.csv', 'r') as file:
+        reader = csv.reader(file)
+        user_data = list(reader)
+    return user_data
+
+def save_user_data(user_data):
+    # Save updated user data to the CSV file
+    with open('datauser.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(user_data)
+
+def login():
+    email = input ("enter your email:")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    user_data = load_user_data()
+
+    for user in user_data:
+        if user[0] == username and user == email [1] and user[2] == password:
+            return True
+
+    return False
 
 def signup():
-    email = input ("masukkan email anda:")
-    username = input("Masukkan username baru: ")
-    password = input("Masukkan password baru: ")
+    email = input ("enter your email:")
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
 
-    # Simpan informasi akun pengguna baru ke dalam database atau file
-    # ...
+    user_data = load_user_data()
 
-    print("Akun berhasil dibuat. Silakan login.")
+    for user in user_data:
+        if user[0] == username:
+            print("Username already exists. Please try again.")
+            return
 
-# Pilihan menu
-print("Selamat datang!")
+    user_data.append([username, password])
+    save_user_data(user_data)
+    print("Account created successfully. Please sign in.")
+
+# Menu
+print("Welcome!")
 print("1. Sign In")
 print("2. Sign Up")
 
-choice = input("Pilih opsi: ")
+choice = input("Choose an option: ")
 
 if choice == "1":
     if login():
-        print("Login berhasil!")
-        # Panggil fungsi atau tampilkan menu setelah login berhasil
+        print("Login successful!")
+        # Call other functions or display menu after successful login
     else:
-        print("Username atau password salah.")
-        # Panggil fungsi login kembali atau berikan pesan error
+        print("Invalid username or password.")
+        # Call login function again or display error message
 elif choice == "2":
     signup()
 else:
-    print("Pilihan anda tidak valid.")
+    print("Invalid option.")
